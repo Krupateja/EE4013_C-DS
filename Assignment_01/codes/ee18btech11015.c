6df30a3320e069ec52a080f82abc31ee612c3470
Krupateja <61040774+Krupateja@users.noreply.github.com>
2021-09-06 12:50:18 +0530

    Update ee18btech11015.c

--- Assignment_01/codes/ee18btech11015.c
+++ Assignment_01/codes/ee18btech11015.c
@@ -1,1 +1,38 @@
 
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node{
+    int f1,f2;
+};
+
+int main() {
+    // Write C code here
+    struct node y = (struct node)malloc(sizeof(struct node));
+    // y->f1 = 1;
+    // y->f2 = 2;
+    int x,z,p,q,i;
+    // x = 0;
+    // p = 0;
+    // q = 0;
+    int t1,t2,t3;
+    
+    t1 = x+3;
+    t2 = y->f1;
+    t3 = y->f2;
+    
+    z = t1+t2+t3;
+    for(i=0;i<200;i++){
+        if(z>i){
+            p += t1;
+            q += t3;
+        }else{
+            p += t3;
+            q += t1;
+        }
+    }
+    // printf("%d-%d-%d-%d-%d-%d\n",x,y->f1,y->f2,z,p,q);
+    
+    return 0;
+}
